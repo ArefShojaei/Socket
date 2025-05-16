55ea17a1935b8c37d9cf51e6bc26cfee0e3c0ad7
ArefShojaei <arefshojaei82dev@gmail.com>
2025-05-16 18:01:05 +0430

    UPDATE: update to add whitespace in the first of message

--- src/Log.php
+++ src/Log.php
@@ -9,6 +9,6 @@ final class Log implements ILog {
     public const INFO = "INFO";
 
     public static function info(string $message): void {
-        echo "[" . self::INFO . "]". "{$message}" . PHP_EOL;
+        echo "[" . self::INFO . "]". " {$message}" . PHP_EOL;
     }
 }
